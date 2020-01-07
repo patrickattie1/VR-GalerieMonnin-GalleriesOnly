90754c19f20f47e8d168a31c8c14890267167f95
Patrick ATTIE <patrick.attie@esih.edu>
2020-01-07 12:06:47 -0500

    Commit Version14 - January 7th, 2020 - Working version for Oculus Quest and Hand Controllers - Portal mechanics and visuals improved. Almost working!

---
+++ Assets/Content/Scripts/PortalTeleporter.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script is attached to each of the collider planes
+public class PortalTeleporter : MonoBehaviour
+{
+    //We need a reference to the Player (here the VRRig)
+    public Transform player; //Make sure vrRig is tagged as player
+
+    //We also need to know where we should teleport our player
+    public Transform receiverPortal;
+
+    private bool playerIsOverlapping = false;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (playerIsOverlapping)
+        {
+            Debug.Log("playerIsOverlapping is true");
+            //We need to make sure the Player is entering the portal from the right side.
+            //We will use the dot product to achieve this.
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            Debug.Log("DP is: " + dotProduct);
+
+            if (dotProduct < 0f) // The Player has just crossed the portal
+            {
+                Debug.Log("DP is negative");
+                //****Fix the rotation
+                //Align the player with the orientation of the destination portal
+                float rotationDifference = -Quaternion.Angle(transform.rotation, receiverPortal.rotation);
+                rotationDifference += 180;
+                //Entering the initial portal from the front and arriving behind the destination portal makes a better effect (180 degrees rotation)
+                player.Rotate(Vector3.up, rotationDifference);
+
+                //****Do the actual translation
+                //Current offset between the player and the start portal. Make sure it is also rotated when applied.
+                //  (this vector has already been calculated before with the portalToPlayer vector).
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDifference, 0f) * portalToPlayer;
+                player.position = receiverPortal.position + positionOffset;
+                Debug.Log("player.position is: " + player.position);
+
+                playerIsOverlapping = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Bool is: " + playerIsOverlapping);
+
+        if (other.tag == "Player")
+        {
+            Debug.Log("tag is Player");
+            playerIsOverlapping = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = false;
+        }
+    }
+}
